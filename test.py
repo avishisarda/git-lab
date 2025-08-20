a7b8601ef04410c2c2cc28d6fc28390d314caf44
AvishiS <avishi.sarda@gmail.com>
2025-08-20 10:08:16 +0530

    my first file commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# test.py
+def add(a, b):
+    return a + b
+
+def main():
+    x = 10
+    y = 20
+    print("The sum of", x, "and", y, "is:", add(x, y))
+
+if __name__ == "__main__":
+    main()
+
